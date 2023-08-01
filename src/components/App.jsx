a92cc03ed3a94205b7c009e2649b7cbeea528a58
Valeriya Pavlova <vla.valeriya@gmail.com>
2023-08-01 16:06:50 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import {
-  BrowserRouter as Router,
-  NavLink,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Home from '../pages/Homepage/Homepage';
 // import Movies from 'pages/Movies/Movies';
 // import MovieDetails from 'pages/MovieDetails/MovieDetails';
